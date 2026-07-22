d7ec8c5f7f9f19e0f7c81c3f5017591f60b88027
Jkz2025 <welivefromart@gmail.com>
2026-07-21 23:27:22 -0500

    a

--- src/lib/AuthContext.jsx
+++ src/lib/AuthContext.jsx
@@ -42,7 +42,7 @@ const signInWithGoogle = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: window.location.origin + '/auth/callback', 
+      redirectTo: window.location.origin + '/', 
       // Esto dará 'https://tusitio.netlify.app/auth/callback'
       skipBrowserRedirect: false,
     },
